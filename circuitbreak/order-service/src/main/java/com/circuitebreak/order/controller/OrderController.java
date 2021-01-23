649adc446293936ebfafc3db0536ab1088c0693e
Sampath <sampath.dapanadurage@ncs.com.sg>
2021-01-23 11:31:12 +0800

    initial commit for circuit breaker pattern with resilience

---
+++ circuitbreak/order-service/src/main/java/com/circuitebreak/order/controller/OrderController.java
@@ -0,0 +1,43 @@
+package com.circuitebreak.order.controller;
+
+import com.circuitebreak.order.model.OrderRequest;
+import com.circuitebreak.order.model.OrderResponse;
+import com.circuitebreak.order.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+@RequestMapping(value = "orders")
+@RequiredArgsConstructor
+public class OrderController {
+    private final OrderService orderService;
+
+    @CircuitBreaker(name = "inventoryBreak", fallbackMethod = "placeOrderFallback")
+    @ApiOperation(value = "Order Service: Place the order")
+    @PostMapping
+    public ResponseEntity<OrderResponse> placeOrder(@RequestBody OrderRequest orderRequest) {
+        return ResponseEntity.ok().body(this.orderService.placeOrder(orderRequest));
+    }
+
+
+    private ResponseEntity<OrderResponse> placeOrderFallback(OrderRequest orderRequest, Throwable throwable) {
+        System.out.println("Inside placeOrderFallback method in resource layer");
+        final OrderResponse response = OrderResponse.builder()
+                .productCode(orderRequest.getProductCode())
+                .productName(orderRequest.getProductName())
+                .count(orderRequest.getCount())
+                .orderId("111111")
+                .totalPrice(new BigDecimal(0))
+                .discount(new BigDecimal(0))
+                .build();
+        return ResponseEntity.ok().body(response);
+    }
+}
---
+++ circuitbreak/order-service/src/main/java/com/circuitebreak/order/service/impl/OrderServiceImpl.java
@@ -0,0 +1,64 @@
+package com.circuitebreak.order.service.impl;
+
+import com.circuitebreak.order.config.ApplicationProperties;
+import com.circuitebreak.order.model.InventoryRequest;
+import com.circuitebreak.order.model.InventoryResponse;
+import com.circuitebreak.order.model.OrderRequest;
+import com.circuitebreak.order.model.OrderResponse;
+import com.circuitebreak.order.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceImpl implements OrderService {
+    private final RestTemplate restTemplate;
+    private final ApplicationProperties applicationProperties;
+
+//    @CircuitBreaker(name = "inventoryBreak", fallbackMethod = "placeOrderFallback")
+    @Override
+    public OrderResponse placeOrder(OrderRequest orderRequest) {
+        InventoryRequest request = InventoryRequest.builder()
+                .productCode(orderRequest.getProductCode())
+                .productName(orderRequest.getProductName())
+                .count(orderRequest.getCount())
+                .orderId(UUID.randomUUID().toString())
+                .build();
+
+        HttpHeaders header = new HttpHeaders();
+        HttpEntity<InventoryRequest> entity = new HttpEntity<>(request, header);
+        ResponseEntity<InventoryResponse> responseEntity = this.restTemplate.exchange(applicationProperties.getInventoryEndpoint(),
+                HttpMethod.POST, entity, InventoryResponse.class);
+        InventoryResponse inventoryResponse = responseEntity.getBody();
+
+        return OrderResponse.builder()
+                .productCode(inventoryResponse.getProductCode())
+                .productName(inventoryResponse.getProductName())
+                .count(inventoryResponse.getCount())
+                .orderId(inventoryResponse.getOrderId())
+                .totalPrice(inventoryResponse.getTotalPrice())
+                .discount(inventoryResponse.getDiscount())
+                .build();
+    }
+
+    private OrderResponse placeOrderFallback(OrderRequest orderRequest, Throwable throwable) {
+        System.out.println("Inside placeOrderFallback method");
+        return OrderResponse.builder()
+                .productCode(orderRequest.getProductCode())
+                .productName(orderRequest.getProductName())
+                .count(orderRequest.getCount())
+                .orderId("111111")
+                .totalPrice(new BigDecimal(0))
+                .discount(new BigDecimal(0))
+                .build();
+    }
+}
